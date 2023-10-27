643771298e33807f47dd7e1c81ee44445188331b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-27 22:30:49 +0400

    feat: react router dom,folder structure init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./global.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./_root/pages";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
 const App = () => {
   return (
-    <div>
-      <h1 className="font-bold underline">App</h1>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   );
 };
 
