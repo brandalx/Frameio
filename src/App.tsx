ebf864866149331aa6d929391db5d6b92205a2b5
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-27 22:34:45 +0400

    feat: layouts for private and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./_root/pages";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
         {/* public routes */}
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-up" element={<SignupForm />} />
+
         {/* private routes */}
-        <Route index element={<Home />} />
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
