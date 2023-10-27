0dc2554cfbda411363de545c611d0924e62729c2
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-27 22:16:47 +0400

    feat: tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./global.css";
 const App = () => {
-  return <div className="font-bold">App</div>;
+  return (
+    <div>
+      <h1 className="font-bold underline">App</h1>
+    </div>
+  );
 };
 
 export default App;
