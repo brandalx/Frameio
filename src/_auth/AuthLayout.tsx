314260b9998cd140d6f7304e273e9cbaaffbc9cc
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-29 03:39:39 +0400

    update: side image

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -12,7 +12,7 @@ const AuthLayout = () => {
           <section className="flex flex-1 justify-center items-center flex-col py-10 ">
             <Outlet />
           </section>
-          <img src="" />
+          <img src="/assets/images/side-img.svg" />
         </>
       )}
     </>
