13ce7ee294548a3031e9e7df228140b5b52eb905
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-29 03:59:05 +0400

    feat: styles for cover image

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -12,7 +12,11 @@ const AuthLayout = () => {
           <section className="flex flex-1 justify-center items-center flex-col py-10 ">
             <Outlet />
           </section>
-          <img src="/assets/images/side-img.svg" />
+          <img
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+            src="/assets/images/side-img.svg"
+          />
         </>
       )}
     </>
