2a43c553fab6a1b1400863e87011864554eb3bea
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-29 04:10:34 +0400

    feat: SignUpValidation schema

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import * as z from "zod";
 import {
   Form,
   FormControl,
@@ -14,21 +13,19 @@ import {
 } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
+import { SignUpValidation } from "@/lib/validation";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
 const SignupForm = () => {
   //  Form defenition.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignUpValidation>>({
+    resolver: zodResolver(SignUpValidation),
     defaultValues: {
       username: "",
     },
   });
 
   // submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignUpValidation>) {
     console.log(values);
   }
 
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,5 @@
+import * as z from "zod";
+
+export const SignUpValidation = z.object({
+  username: z.string().min(2).max(50),
+});
