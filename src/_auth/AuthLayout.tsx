4530b5955e3df5e38a97db2155294f2af56bc59a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-29 04:05:49 +0400

    feat: form defenition and handler with zod, rhf

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+
 import { Outlet, Navigate } from "react-router-dom";
+
 const AuthLayout = () => {
   const isAuthenticated = false;
 
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,25 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
-
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
 const SignupForm = () => {
+  //  Form defenition.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <div>
       <Button>Button</Button>
