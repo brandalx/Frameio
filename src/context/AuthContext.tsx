fd08498814d5d8aa64bde42b0d8ecb4d393e5a54
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-29 15:45:43 +0400

    feat: createcontext stets, props, initial vals

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,6 @@
+import { IUser } from "@/types";
 import { createContext, useContext, useEffect, useState } from "react";
-
+import React from "react";
 export const INITIAL_USER = {
   id: "",
   name: "",
@@ -15,14 +16,29 @@ const INITIAL_STATE = {
   isAutheticated: false,
   error: null,
   setUser: () => {},
-  iseIsAutheticated: () => {},
+  IsAutheticated: () => {},
   checkAuthUser: async () => false as boolean,
 };
 
-import React from "react";
+const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isLoading, setIsloading] = useState(false);
+
+  const [isAutheticated, setIsAutheticated] = useState(false);
+
+  const checkAuthUser = () => {};
+
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    isAutheticated,
+    setIsAutheticated,
+    checkAuthUser,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
-export default AuthContext;
+export default AuthProvider;
