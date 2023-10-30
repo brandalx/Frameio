fc4b7916cbb5c961578f6b0e9f25dbf520f8955b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-30 11:35:17 +0400

    feat:routes pages init

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,14 @@ import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "./components/ui/toaster";
+import Explore from "./_root/pages/Explore";
+import Saved from "./_root/pages/Saved";
+import AllUsers from "./_root/pages/AllUsers";
+import CreatePosts from "./_root/pages/CreatePosts";
+import EditPost from "./_root/pages/EditPost";
+import PostDetails from "./_root/pages/PostDetails";
+import Profile from "./_root/pages/Profile";
+import UpdateProfile from "./_root/pages/UpdateProfile";
 const App = () => {
   return (
     <main className="flex h-screen">
@@ -24,7 +32,7 @@ const App = () => {
           <Route path="/explore" element={<Explore />} />{" "}
           <Route path="/saved" element={<Saved />} />{" "}
           <Route path="/all-users" element={<AllUsers />} />{" "}
-          <Route path="/create-post" element={<CreatePost />} />{" "}
+          <Route path="/create-post" element={<CreatePosts />} />{" "}
           <Route path="/update-post/:id" element={<EditPost />} />{" "}
           <Route path="/post/:id" element={<PostDetails />} />{" "}
           <Route path="/profile/:id/*" element={<Profile />} />{" "}
