656eb3e1103d077904219ed66702943777bd132b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-30 08:31:56 +0400

    feat: sign out, profile image

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,8 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
 
+import { useToast } from "../ui/use-toast";
+import { useUserContext } from "@/context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+
 const Topbar = () => {
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { toast } = useToast();
+  const { user } = useUserContext();
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+
+      toast({
+        variant: "default",
+        title: "Sign Out success",
+        description: "See you soon at Frameio!",
+      });
+    }
+  }, [isSuccess]);
+
   return (
     <section className="topbar">
       <div className="flex-between py-4 px-5 ">
@@ -17,11 +37,18 @@ const Topbar = () => {
         <div className="flex gap-4 ">
           <Button
             variant="ghost"
-            className="shad-button_ghost"
-            onClick={signOut}
+            className="shad-button_ghost hover:opacity-50 transition-all"
+            onClick={() => signOut()}
           >
             <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3 ">
+            <img
+              src={user.imageUrl || "/assets/images/profile.png"}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
         </div>
       </div>
     </section>
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,11 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+import {
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
 import { INewUser } from "@/types";
 
 export const useCreateUserAccount = () => {
@@ -19,8 +23,8 @@ export const useSignInAccount = () => {
   });
 };
 
-export const useSignOut = () => {
+export const useSignOutAccount = () => {
   return useMutation({
-    mutationFn: () => signOutAccount,
+    mutationFn: signOutAccount,
   });
 };
