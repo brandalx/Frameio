a8d0d3946880741c9899146fe03f9a933e80090d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-30 11:52:16 +0400

    feat: bottom navbar stylyzed

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -9,23 +9,22 @@ const Bottombar = () => {
       {bottombarLinks.map((link) => {
         const isActive = pathname === link.route;
         return (
-          <li
-            className={`leftsidebar-link group transition-all ${
-              isActive && "bg-primary-500"
-            }`}
+          <Link
+            to={link.route}
+            className={`  ${
+              isActive && "bg-primary-500 rounded-[10px] "
+            } flex-center flex-col gap-1 p-2 transition`}
             key={link.label}
           >
-            <Link to={link.route} className="flex gap-4 items-center p-4">
-              <img
-                src={link.imgURL}
-                alt={link.label}
-                className={`group-hover:invert-white ${
-                  isActive && "invert-white"
-                }`}
-              />
-              {link.label}
-            </Link>
-          </li>
+            <img
+              width={16}
+              height={16}
+              src={link.imgURL}
+              alt={link.label}
+              className={` ${isActive && "invert-white"}`}
+            />
+            <p className="tiny-medium text-light-2 "> {link.label}</p>
+          </Link>
         );
       })}
     </section>
