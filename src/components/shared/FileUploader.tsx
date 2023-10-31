3a321260bd590a222010a2120b12b52f9869109a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-31 15:11:00 +0400

    feat: file uploader dropzone basic layout and init

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 const FileUploader = () => {
-  return <div>FileUploader</div>;
+  const [fileUrl, setfileUrl] = useState("");
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  return (
+    <div
+      {...getRootProps()}
+      className="flex flex-center bg-dark-3  rounded-xl cursor-pointer"
+    >
+      <input {...getInputProps()} className="cursor-pointer " />
+      {fileUrl ? <div>test 1</div> : <div>test 2</div>}
+    </div>
+  );
 };
 
 export default FileUploader;
