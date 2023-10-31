bac3c94b9b12c59d719e20fd73dbddebe713a7a1
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-31 15:28:15 +0400

    feat: dropzone ui, receiving data

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -5,6 +5,9 @@ const FileUploader = () => {
   const [fileUrl, setfileUrl] = useState("");
   const onDrop = useCallback((acceptedFiles) => {
     // Do something with the files
+    // setfileUrl(acceptedFiles);
+    console.log(acceptedFiles);
+    setfileUrl(acceptedFiles[0].path);
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
@@ -13,7 +16,22 @@ const FileUploader = () => {
       className="flex flex-center bg-dark-3  rounded-xl cursor-pointer"
     >
       <input {...getInputProps()} className="cursor-pointer " />
-      {fileUrl ? <div>test 1</div> : <div>test 2</div>}
+      {fileUrl ? (
+        <div>{fileUrl}</div>
+      ) : (
+        <div className="file_uploader-box">
+          <img
+            src="/assets/icons/file-upload.svg"
+            width={96}
+            height={77}
+            alt="file-upload"
+          />
+          <h3 className="base-medium text-light-2 mb-2 mt-6">
+            Drag photo here
+          </h3>
+          <p className="text-light-4 small-regular mb-6">PNG, JPG</p>
+        </div>
+      )}
     </div>
   );
 };
