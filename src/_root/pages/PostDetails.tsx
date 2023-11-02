b73ab6ad491c329600a290881da2d17190e6e92b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-02 15:04:53 +0400

    feat:initial layout with data fetching postdetails

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,7 +1,50 @@
+import Loader from "@/components/shared/Loader";
+import { formatDate } from "@/lib/helpers/formatDate";
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 const PostDetails = () => {
-  return <div>PostDetails</div>;
+  const { id } = useParams();
+
+  const { data: post, isPending } = useGetPostById(id || "");
+  console.log(post);
+  return (
+    <div className="post_details-container">
+      {isPending ? (
+        <Loader />
+      ) : (
+        <div className="post_details-card">
+          <div className="flex items-center gap-3 ">
+            <Link to={`/profile/${post?.creator.$id}`}>
+              <img
+                src={
+                  post?.creator?.imageUrl ||
+                  "/assets/icons/profile-placeholder.svg"
+                }
+                alt="Creator"
+                className="rounded-full w-12lg: h-12"
+              />
+            </Link>
+            <div className="flex flex-col ">
+              <p className="base-medium lg:body-bold text-light-1 ">
+                {post?.creator.name}
+              </p>
+              <div className="flex-center gap-2 text-light-3 ">
+                <p className="subtle-semibold lg:small-regular">
+                  {formatDate(post?.$createdAt || "")}
+                </p>
+                -
+                <p className="subtle-semibold lg:small-regular">
+                  {post?.location}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PostDetails;
