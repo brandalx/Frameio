f8c6f384b28dd222344a71aa2d33cbfeb61ac3a9
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-03 16:25:47 +0400

    feat: explore ui init: search, header

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,7 +1,22 @@
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="h3-bold md:h2-bold w-full"> Search Posts</h2>
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+          <img src="/assets/icons/search.svg" alt="Search" />
+          <Input
+            type="search"
+            className="explore-search "
+            placeholder="Search"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Explore;
