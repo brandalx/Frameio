a87ae3af41076724f50eed9185e64c07e9894a6a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-03 16:56:51 +0400

    feat:explore layout SearchResults GridPostList in

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,7 +1,17 @@
+import GridPostList from "@/components/shared/GridPostList";
+import SearchResults from "@/components/shared/SearchResults";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useState } from "react";
 
 const Explore = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const posts = [];
+  const shouldShowSearchResults = searchValue !== "";
+
+  const shouldShowPosts =
+    !shouldShowSearchResults &&
+    posts?.pages.every((item) => item.documents.keys.length === 0);
+
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
@@ -9,11 +19,36 @@ const Explore = () => {
         <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
           <img src="/assets/icons/search.svg" alt="Search" />
           <Input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
             type="search"
             className="explore-search "
             placeholder="Search"
           />
         </div>
+      </div>
+      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h2 className="body-bold md:h3-bold"> Popular Today </h2>
+        <div className="flex-center gap-3 bg-dark-4 rounded-xl px-4 py-2 cursor-pointer hover:bg-dark-3 transition-all">
+          <p className="small-medium md:base-medium text-light-2">All</p>
+          <img
+            src="/assets/icons/filter.svg"
+            width={20}
+            height={20}
+            alt="filter"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+        {shouldShowSearchResults ? (
+          <SearchResults />
+        ) : shouldShowPosts ? (
+          <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
+        ) : (
+          posts.pages.map((item, index) => (
+            <GridPostList key={`page-${index}`} posts={item} />
+          ))
+        )}
       </div>
     </div>
   );
